781097a6ed23b5d12f99a8b2a508566f946d33b8
HanWenLu <38583116+hanwenlu2016@users.noreply.github.com>
2020-12-30 14:25:11 +0800

    调整函数调用
    调整函数调用

--- main/run.py
+++ main/run.py
@@ -5,11 +5,9 @@
 # @Time: 2020/10/26  19:04
 
 import pytest
-import os
-from config.setting import PRPORE_ALLURE_DIR, PRPORE_JSON_DIR, PRPORE_SCREEN_DIR, IS_CLEAN_REPORT
-from config.setting import CASE_DIR
+from config.setting import *
 from public.logs import logger
-from public.cleanup import clean_report
+from public.cleanup import del_clean_report
 
 
 def run():
@@ -17,11 +15,10 @@ def run():
     运行所有测试用例
     :return: noul
     """
-    if IS_CLEAN_REPORT == True:
-        dir_list = [PRPORE_ALLURE_DIR, PRPORE_JSON_DIR, PRPORE_SCREEN_DIR]
 
-        for dir in dir_list:
-            clean_report(dir)
+    # 执行前检查是否清除报告
+    del_clean_report()
+
 
     # 执行用列
     #pytest.main(['-s', '-v', '-m', 'me', '-n=1', '--alluredir', f'{PRPORE_JSON_DIR}', f'{CASE_DIR}'])
@@ -35,5 +32,3 @@ def run():
 
 if __name__ == '__main__':
     run()
-
-
