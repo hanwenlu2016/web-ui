7eb9115bb2c1200171cd51dc76fa5e3691ce38a5
HanWenLu <38583116+hanwenlu2016@users.noreply.github.com>
2021-03-26 15:25:53 +0800

    调整架构目录！！！
    调整架构目录！！！

--- config/setting.py
+++ config/setting.py
@@ -5,77 +5,67 @@
 # @Time: 2020/10/28  9:20
 
 
-import  os ,sys
+########## 通用配置
+CASE_TYPE = 'web'  # 项目类型  'app'  # web
+
+IS_CLEAN_REPORT = True  # 是否清除 测试历史测试报告结果 * 默认清除
+
+IS_REDIS = False  # 是否读取redis 数据 (完成代码用列后导入管理平台时使用) 默认不读取 #False/True
+
+########## selenium/appium配置
+IMPLICITLY_WAIT_TIME = 10  # 显示等待最长时间 /s
+
+POLL_FREQUENCY = 0.2   # 显示等待元素出现时 在此时间内检索一次 /s
 
 
-# 运行用列类型 (app、web)
-CASE_TYPE ='app' #'app'  # web
+########## Web端配置
+URL = "http://192.168.7.51/portal/#"  # # 项目地址 web 时选择
 
-# -----selenium/appium  配置
-# 显示等待最长时间 /s
-IMPLICITLY_WAIT_TIME = 10
+BROWSERNAME = "Chrome"  # "Chrome" "Firefox" "Ie"  "Safari"  # 浏览器选择
 
-# 显示等待元素出现时 在此时间内检索一次 /s
-POLL_FREQUENCY = 0.2
+IS_COLONY = False  # 是否启动集群模式 True(启用)  False(不启用)
 
-
-# 是否清除 测试历史测试报告结果 * 默认清除
-IS_CLEAN_REPORT = True
-
-# 是否读取redis 数据 (完成代码用列后导入管理平台时使用) 默认不读取
-IS_REDIS = False
-
-# 框架基础路径  不需要修改
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# 日志路径
-LOG_DIR = os.path.join(BASE_DIR, "log")
-
-### 测试用例集路径
-# APP/WEB用列目录
-APP_CASE_DIR = os.path.join(BASE_DIR, "case", "appcase")
-WEB_APP_CASE_DIR = os.path.join(BASE_DIR, "case", "webcase")
-
-# 如果用列类型等于app 就返回app的用列目录 否则返回web的
-CASEPATH = lambda: APP_CASE_DIR if CASE_TYPE.lower() == 'app' else WEB_APP_CASE_DIR
-CASE_DIR = CASEPATH()
-
-### yaml测试用列数据路径
-# APP/WEB YAML数据
-APP_YAML_DIR = os.path.join(BASE_DIR, "database", "yaml","app", )
-WEB_YAML_DIR = os.path.join(BASE_DIR, "database","yaml" ,"web" )
-
-# 如果用列类型等于app 就返回app的YAML 否则返回web的
-DATA_YAML = lambda: APP_YAML_DIR if CASE_TYPE.lower() == 'app' else WEB_YAML_DIR
-YAML_DIR = DATA_YAML()
+HUB_HOST = '192.168.7.101:4444'  # 集群hub地址和端口
 
 
+########## 移动端配置
+PLATFORM = 'ios'  #'Android'  # ios Android  平台 app 时配合
+
+APIUMHOST = '192.168.203.17:4723'  # appium 服务器地址
+
+ANDROID_CAPA = {"platformName": PLATFORM,
+                "platformVersion": "6.0.1", # 系统版本
+                "deviceName": 'emulator-5554', # 设备名称
+                "appPackage": "com.android.settings", # app包名
+                "appActivity": ".Settings", # 启动页面 *
+                "autoLaunch": True,  # 测试时是否需要自动运行app  * 必须要开启
+                "autoGrantPermissions": True,  # 让appium自动授权app权限，如果noReset为True，则该条不生效。
+                "newCommandTimeout": 60,  # 为了结束Appium会话，会设置一个等待从客户端发送命令的超时时间，默认为60秒，一般不需要设置
+                "deviceReadyTimeout": 60,  # 等待设备就绪的时间
+                "noReset": True, # 启动app时不要清除app里的原有的数据
+                } # 安卓参数
+
+IOS_CAPA = {
+    "platformName": PLATFORM, # *
+    "platformVersion": '14.1', # 系统版本 *
+    "deviceName": 'Redad', #设备名称 *
+    "automationName": "XCUITest",
+    "bundleId": "com.redaflight.RedaSkyPoint22a", # app包名 *
+    "udid": "5235c499f02839759fccc412fdb4c50920ef5244", # 手机UDID *
+    "xcodeOrgId": "J4397KG4TG", # 开发者id *
+    "xcodeSigningId": "iPhone Developer",
+    "useNewWDA": False,
+    "noReset": True, # 启动app时不要清除app里的原有的数据
+    "newCommandTimeo": 60
+}  # IOS参数
 
 
-### 测试文件路径
-# APP/WEB flie数据
-DATA_FILE_APP = os.path.join(BASE_DIR, "database", "file", "app")
-DATA_FILE_WEB = os.path.join(BASE_DIR, "database", "file", "web")
-
-# 如果用列类型等于app 就返回app的测试文件 否则返回web的
-DATA_FILE = lambda: DATA_FILE_APP if CASE_TYPE.lower() == 'app' else DATA_FILE_WEB
-DATA_DIR = DATA_FILE()
-
-# 测试用例结果目录
-PRPORE_JSON_DIR = os.path.join(BASE_DIR, "output", "report_json")
-
-# 测试结果报告目录
-PRPORE_ALLURE_DIR = os.path.join(BASE_DIR, "output", "report_allure")
-
-# 测试截图目录
-PRPORE_SCREEN_DIR = os.path.join(BASE_DIR, "output", "report_screen")
-
-# 数据库信息配置
-MYSQL = {'user': 'root', 'password': 'root', 'port': 3306, 'host': '127.0.0.1', 'db': 'fdr'}
+########## DB 配置
+MYSQL = {'user': 'root', 'password': 'root', 'port': 3306, 'host': '127.0.0.1', 'db': 'redamanage'}
 
 ORACLE = {'user': 'cdm', 'password': 'cdm', 'host': '192.168.7.158:1521/ORCL'}
 
-REDIS = {'host': '192.168.203.5', 'port': '6379', 'password': '', 'db': 1}
+REDIS = {'host': '127.0.0.1', 'port': '6379', 'password': '', 'db': 1}
 
 # redis 集群配置
 REDIS_CLUSTER = [{'host': '192.168.7.51', 'port': 7000}, {'host': '192.168.7.52', 'port': 7000},
@@ -84,7 +74,6 @@ REDIS_CLUSTER = [{'host': '192.168.7.51', 'port': 7000}, {'host': '192.168.7.52'
 
 REDIS_CLUSTER_PASWORD = 'reda2019'
 
-# 打码平台API信息  页面需要登录验证码时 启用
-# 账号 密码 打码软件id api地址
-IMG_INFO = {'username': '', 'password': '', 'code_id': 909536,
-            'api_url': 'http://upload.chaojiying.net/Upload/Processing.php'}
+
+
+
