889e55b365eaa3dec6256e1270abb4ac823df8f3
HanWenLu <38583116+hanwenlu2016@users.noreply.github.com>
2021-03-26 15:30:18 +0800

    调整架构目录
    调整架构目录

---
+++ pageobj/app_jd.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# @File: jd.py
+# @Author: HanWenLu
+# @E-mail: wenlupay@163.com
+# @Time: 2021/1/14  16:02
+
+import time
+
+from public.yaml_data import GetCaseData
+from public.app_base import AppBase
+
+
+
+class Jd(AppBase):
+    """
+    京东demo
+    """
+
+    def openjd(self):
+        jd = GetCaseData('app_jd.yaml', 'openjd')
+
+        time.sleep(2)
+        self.used_click(types=jd.types(0), locate=jd.locate(0))
+
+        relust = self.used_text(types=jd.types(1), locate=jd.locate(1))
+        time.sleep(1)
+
+        assert relust == jd.expect(1)
