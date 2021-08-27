e73929b9ef30e3cbf7ff5e519d1f767b675eec62
kadingzz1 <77272159+kadingzz1@users.noreply.github.com>
2021-08-27 22:11:08 +0800

    解决日志不写入log文件

--- public/common.py
+++ public/common.py
@@ -122,10 +122,12 @@ class SetLog:
 
     ERR_LOG_PATH = os.path.join(LOG_DIR, f'{DAY}_err.log')
 
+    logger.remove()  # 删去import logger之后自动产生的handler，不删除的话会出现重复输出的现象
+    
     logger.add(LOG_PATH, rotation="00:00", encoding='utf-8')
 
     logger.add(ERR_LOG_PATH, rotation="00:00", encoding='utf-8', level='ERROR', )
-    logger.remove()  # 删去import logger之后自动产生的handler，不删除的话会出现重复输出的现象
+    
     handler_id = logger.add(sys.stderr, level=LEVEL)  # 添加一个可以修改控制的handler
 
 
