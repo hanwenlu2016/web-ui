ef4758f3f2a29a5a529213ca1c4bb0c0974818b1
HanWenLu <38583116+hanwenlu2016@users.noreply.github.com>
2022-03-31 17:45:32 +0800

    增加demo

--- public/driver_init.py
+++ public/driver_init.py
@@ -12,7 +12,6 @@ import requests
 
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.chrome.service import Service
 
 from public.common import ErrorExcep, logger
 from config.ptahconf import WIN_CHROMEDRIVER, LUINX_CHROMEDRIVER, MAC_CHROMEDRIVER
