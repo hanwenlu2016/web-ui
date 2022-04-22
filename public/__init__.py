20e86aecf36cf87f89cb183d037b6f2bbcad56b4
HanWenLu <38583116+hanwenlu2016@users.noreply.github.com>
2022-04-22 10:05:11 +0800

    修复oracle 插入数据问题

--- public/__init__.py
+++ public/__init__.py
@@ -1,8 +1,7 @@
-from api_base import ApiBase, apiexe
-from app_base import Base, CommonlyUsed, AndroidUiautomatorBase, IosPredicate, AccessibilityId, AppBase
-from web_base import Base,WebBase,AutoRunCase
+from public.api_base import ApiBase, apiexe
+from public.app_base import Base, CommonlyUsed, AndroidUiautomatorBase, IosPredicate, AccessibilityId, AppBase
+from public.web_base import Base,WebBase,AutoRunCase
 
-# from public import ApiBase
 
 __all__=['ApiBase','apiexe','Base','CommonlyUsed','AndroidUiautomatorBase','IosPredicate','AccessibilityId','AppBase',
         'Base','WebBase','AutoRunCase'
